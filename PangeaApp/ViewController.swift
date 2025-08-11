55c9b18dc5046667c5b81a4eee1f81c2555f0e16
Larisa Clemenceau <larisa@eucledian.com>
2025-08-11 11:07:39 +0200

    Initial Commit

---
+++ PangeaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PangeaApp
+//
+//  Created by Larisa Clemenceau on 11/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
